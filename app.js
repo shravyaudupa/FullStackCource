258fd0c0f54ae3c245526f6925b81c6c1b3d71d7
shravyaudupa <shravyaudupa912@gmail.com>
2021-09-28 18:56:21 +0530

    Dta types

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log( typeof "Hello World")
+console.log( typeof "d")
+
+
+console.log( typeof 88)
+console.log( typeof 1.6)
+console.log( typeof -1.6)
+
+console.log( typeof true)
+console.log( typeof false)
