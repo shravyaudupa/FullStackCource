b02c93303847507ad87a6f8529045d6d2ab481b8
shravyaudupa <shravyaudupa912@gmail.com>
2021-09-28 19:45:35 +0530

    operators

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
-var arr =[1,2,3,4]
-console.log(arr)
+var a=2
+var b=4
+console.log('a+b=', a+b)
+console.log('a-b=', a-b)
+console.log('a*b=', a*b)
+console.log('a/b=', a/b)
+console.log('a**b=', a**b)
+console.log('a%b=', a%b)
 
-arr.push(6)
-console.log(arr)
-console.log( arr.length)
+
+b = 11
+console.log('b', b)
+b += 11
+console.log('b+=', b)
+
+// comparision 
+
+var d =10
+var e =2
+var f ='10'
+
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+
+console.warn(d == f)
+console.warn(d === f)
+console.warn(d +f)
+// logical
+
+console.log('true && true ', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true ', true || true)
+console.log('true || false', true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
+
+
+
+
+
+
+
