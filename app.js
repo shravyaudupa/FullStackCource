d7e7d1ad1b4d53a19c209ddfe167fea53d451e18
shravyaudupa <shravyaudupa912@gmail.com>
2021-09-28 19:10:54 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-console.log( typeof "Hello World")
-console.log( typeof "d")
-
-
-console.log( typeof 88)
-console.log( typeof 1.6)
-console.log( typeof -1.6)
-
-console.log( typeof true)
-console.log( typeof false)
+var age = null
+console.log(typeof age)
