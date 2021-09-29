6aaf5c7fa72cb091e27691d8dcccd462d35a4f8a
shravyaudupa <shravyaudupa912@gmail.com>
2021-09-29 19:16:23 +0530

    methods

--- app.js
+++ app.js
@@ -1,48 +1,6 @@
-var a=2
-var b=4
-console.log('a+b=', a+b)
-console.log('a-b=', a-b)
-console.log('a*b=', a*b)
-console.log('a/b=', a/b)
-console.log('a**b=', a**b)
-console.log('a%b=', a%b)
+var heading = document.getElementById('heading')
+var para = document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector('.para')
 
-
-b = 11
-console.log('b', b)
-b += 11
-console.log('b+=', b)
-
-// comparision 
-
-var d =10
-var e =2
-var f ='10'
-
-console.log(d > e)
-console.log(d < e)
-console.log(d >= e)
-console.log(d <= e)
-console.log(d == e)
-
-console.warn(d == f)
-console.warn(d === f)
-console.warn(d +f)
-// logical
-
-console.log('true && true ', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('false && false', false && false)
-
-console.log('true || true ', true || true)
-console.log('true || false', true || false)
-console.log('false || true', false || true)
-console.log('false || false', false || false)
-
-
-
-
-
-
-
+console.log(query)
