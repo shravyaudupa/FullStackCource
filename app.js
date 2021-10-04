ab0f4a8be6394783dd8375983b563479c1ec28fd
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-04 19:43:58 +0530

    time

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-let manufacturer = "ssss"
+setTimeout(()=> {
+    console.log("Shravya")
+},3000)
 
-let car = {
-    name: "C Class",
-    manufacturer: "gggg",
-    print: () => {
-       console.log(`${this.name} was created by ${this.manufacturer}`)
-    }
-}
-
-console.log(Object.keys(car))
-console.log(Object.values(car))
