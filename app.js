87481f39605c91e88a375a3da667ac374c85fa94
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-09 06:52:42 -0700

    utils

---
+++ app.js
@@ -0,0 +1,5 @@
+const {add, subtract, multiply, divide,message} = require('./utils')
+add()
+subtract()
+multiply()
+divide()
---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log('something')
+
+
+
+// console.log(document)
+// console.log(window)
+
+
+
+// setTimeout (() => {
+//     console.log('hi')
+// }, 5000)
+
+
+const greet =() => 'hi'
+module.exports.greet = greet
+console.log('shravya')
