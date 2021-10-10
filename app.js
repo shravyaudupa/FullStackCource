1a86f0b6b6e8758bd3ce2bf26b589d2b45d2e6a6
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-10 13:10:44 +0530

    splice vs slice

--- app.js
+++ app.js
@@ -72,6 +72,13 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8]
 // console.log(new_arr)
 
 
-arr.filter((element, index) =>  {
-    console.log(element, index)
-})
+// var new_arr = arr.filter((element, index) =>  {
+//     if(element %2 === 0 ) return true
+// })
+
+// console.log(new_arr)
+
+
+
+console.log(arr.splice(5, 2))
+console.log(arr)
