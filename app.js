5115c25ac7b39d1930d26d3206765c8e0c085d43
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-10 10:36:48 +0530

    add subtract multiply

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const a = document.querySelectorAll('input')[0]
 const b = document.querySelectorAll('input')[1]
 const add = document.querySelector('#add')
 const multiply = document.querySelector('#multiply')
+const subtract = document.querySelector('#subtract')
 const resultBox = document.querySelector('.result')
 
 const sum = () => {
@@ -13,26 +14,42 @@ const product = () => {
     const result= parseInt(a.value) * parseInt(b.value)
     resultBox.innerHTML= result
 }
+const minus = () => {
+    const result= parseInt(a.value) - parseInt(b.value)
+    resultBox.innerHTML= result
+}
 
-const calculate =(op) => {
-    if(op == 'add'){
-        console.log('Adding')
-    } else if (op == 'multiply'){
-        console.log('Multiply')
+const calculate =(event, operation) => {
+    // if(op == 'add'){
+    //     console.log('Adding')
+    // } else if (op == 'multiply'){
+    //     console.log('Multiply')
+    // }
+
+    switch (operation) {
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "multply":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "subtract":
+            resultBox.innerHTML = parseInt(a.value) - parseFloat(b.value)
+            break    
+        default:
+            resultBox.innerHTML = "Not a valid operation"        
     }
 }
 
 
 
-add.addEventListener('click', sum)
-multiply.addEventListener('click', product)
+// add.addEventListener('click', sum)
+// multiply.addEventListener('click', product)
 
 
-add.addEventListener('click', () => {
-    return calculate('add')
-})
+add.addEventListener('click', (event) => { calculate(event,'add')})
 
-multiply.addEventListener('click',() => {
-    return calculate('multiply')
-})
+multiply.addEventListener('click',(event) => { calculate(event,'multiply')})
+
+subtract.addEventListener('click',(event) => { calculate(event,'subtract')})
 
