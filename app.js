8a6a1046aca8b871fdfbb7530c773babaa0b207b
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-11 19:04:22 +0530

    paths

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
 console.log(path.resolve('./app.js'))
-console.log(path.relative('.../','./CSS/util.js'))
+
+// console.log(path.relative('.../','./CSS/util.js'))
+
+// console.log(path.extname('.../','./CSS/util.js'))
+
+// console.log(path.dirname('./CSS/util.js'))
+
+console.log(__dirname)
 
