bff3aa1d52a50c7d66e0f43db54bc0f2538d205a
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-11 18:40:11 +0530

    resolve and relative path

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
+const path = require('path')
 
-// div.innerHTML = localStorage.getItem('Inputvalue')
-
-// button.addEventListener('click', (req, res) => {
-//     localStorage.setItem('Inputvalue', [1, 2, 3, 4])
-//     div.innerHTML = input.value
-// })
-
-
-
-
-const arr = [1,2,3,4,5,6]
-
-// JSON.stringify(arr)
-
-
-
-localStorage.setItem('val', JSON.stringify(arr))
-// console.log(arr)
-console.log(localStorage.getItem('val'))
-
+console.log(path.resolve('./app.js'))
+console.log(path.relative('.../','./CSS/util.js'))
 
