7db48b33442808f0737429b2f912ce543459ba80
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-11 19:22:21 +0530

    read_file_sync

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const fs =require('fs')
-fs.readFile('./text.txt','ucs-2',(error, data)=>{
-    if(error) console.log(error)
-    else console.log( data)
 
-})
+
+// fs.readFile('./text.txt','ucs-2',(error, data)=>{
+//     if(error) console.log(error)
+//     else console.log( data)
+
+// })
+
+console.log(fs.readFileSync('./text.txt', 'utf8'))
+
+console.log("I'm shravya")
