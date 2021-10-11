fff9477b837197cb075bb56e72d16cf9032f20c8
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-11 18:18:13 +0530

    array

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const {add, subtract, multiply, divide,message} = require('./utils')
-add()
-subtract()
-multiply()
-divide()
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+
+// div.innerHTML = localStorage.getItem('Inputvalue')
+
+// button.addEventListener('click', (req, res) => {
+//     localStorage.setItem('Inputvalue', [1, 2, 3, 4])
+//     div.innerHTML = input.value
+// })
+
+
+
+
+const arr = [1,2,3,4,5,6]
+
+// JSON.stringify(arr)
+
+
+
+localStorage.setItem('val', JSON.stringify(arr))
+// console.log(arr)
+console.log(localStorage.getItem('val'))
+
+
