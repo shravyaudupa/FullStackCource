2ade3d5e002d9dd1994792ab60207e5d094c828c
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-15 21:46:48 +0530

    express

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-const http= require('http')
-console.log(http)
+// const http= require('http')
+// const fs = require('fs')
+// const path = require('path')
+// const { url } = require('inspector')
+// // console.log(http)
 
-const server =http.createServer((request,response) => {
-    console.log(request)
+// const server =http.createServer((request,response) => {
+    
+//     const {url} = request
+//     if(url== '/login') {
+//         response.write('<h1>Login</h1>')
+//         response.end()
+//     }
+//     if(url== '/signup') {
+//         response.write('<h1>SignUp</h1>')
+//         response.end()
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log(`Server listening at port : ${3000}`)
+// })
+
+
+
+const data =[{ name: "shravya"}, { name: "sarva"}, { name: "vishwa"}]
+
+const express = require('express')
+const app = express()
+
+app.get('/getNames', (req, res) => {
+    res.send(data)
 })
 
-console.log(server)
+app.listen(3000, () => {
+    console.log("server listening a port 3000")
+})
