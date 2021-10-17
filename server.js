64d50572ae05f1e7af006bf2e821762ce551fb40
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-17 13:50:51 +0530

    headers

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-app.get('/', (req,res)=>{
+const isAdmin = (req, res, next) =>{
+    if(req.headers.admin ==="true") next()
+    else res.send("UNAUTHORISED")
+}
+
+app.get('/public',  (req,res) => {
     console.log(req.headers)
-    res.send('GET')
+    res.send(`I'm a public route`)
+})
+
+app.get('/private', isAdmin, (req,res) => {
+    res.send(`I'm a public route`)
 })
 
 
-app.listen(PORT,() =>{
-    console.log(`Server running at PORT ${3000}`)
+app.listen(PORT,() => {
+    console.log(`Server running at PORT ${PORT}`)
 })
 
 
