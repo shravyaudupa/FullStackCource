aed29c4e2133be040eb37c7e05d61c1c670b8142
shravyaudupa <shravyaudupa912@gmail.com>
2021-10-18 23:01:07 +0530

    body

--- server.js
+++ server.js
@@ -2,20 +2,10 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-const isAdmin = (req, res, next) =>{
-    if(req.headers.admin ==="true") next()
-    else res.send("UNAUTHORISED")
-}
-
-app.get('/public',  (req,res) => {
-    console.log(req.headers)
-    res.send(`I'm a public route`)
+app.post('/signup',(req, res) =>{
+    console.log(req.body)
+    res.send('tttt')
 })
-
-app.get('/private', isAdmin, (req,res) => {
-    res.send(`I'm a public route`)
-})
-
 
 app.listen(PORT,() => {
     console.log(`Server running at PORT ${PORT}`)
